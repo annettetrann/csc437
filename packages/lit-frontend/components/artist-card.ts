f7955d8b0759c7aa18f596ce6c326888b752806a
annettetrann <annettetran5@gmail.com>
2024-02-07 13:39:04 -0800

    created index components

---
+++ packages/lit-frontend/components/artist-card.ts
@@ -0,0 +1,59 @@
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+@customElement("artist-card")
+class ArtistCard extends LitElement {
+    @property({ type: String})
+    name: string = "";
+    
+    @property({ type: String})
+    artistPicPath: string = "";
+    
+    @property({ type: Array})
+    genres: string[] = ["electronica", "house", "techno"];
+
+    
+    render() {
+        return html`
+        <div class="card">
+            <span class="artistImage">
+                <slot name="artistImage">
+            </span>
+            <h3>
+                <slot name="artistName">Artist Name</slot>
+            </h3>
+            <slot name="genres">${this.genres.map((genre) => html`<dd>${genre}</dd>`)}></slot>
+        </card>
+    `;
+}
+    
+static styles = css`
+    :host {
+        display: block;
+        height: 100%;
+        width: auto;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        padding: 20px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        background-color: rgba(255, 255, 255, 0.5);
+    }
+
+    // article {
+    // display: flex;
+    // flex-direction: column;
+    // align-items: center;
+    // gap: var(--size-spacing-medium);
+    // }
+
+    label {
+        cursor: pointer;
+    }
+    
+    img {
+        width: 100%;
+        border-radius: var(--size-corner-medium);
+    }
+    `;
+
+}
