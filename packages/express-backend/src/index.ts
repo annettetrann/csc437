880a88ec593f5a98e4da323450a0e4e34871d47f
annettetrann <annettetran5@gmail.com>
2024-02-14 14:25:19 -0800

    database calls connected and working

--- packages/express-backend/src/index.ts
+++ packages/express-backend/src/index.ts
@@ -1,19 +1,46 @@
 // src/index.ts
-import { connect } from "./mongoConnect";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { connect } from "./mongoConnect";
+
+import profiles from "./profiles";
+import { Profile } from "./models/profile";
+
+
+console.log("Connecting to MongoDB"); 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+console.log("Connecting to MongoDB");
 app.use(cors());
 app.use(express.json());
 
-connect("soundwavecollective")
+console.log("Connecting to MongoDB");
+connect("soundwavecollective");
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
-);
+});
+// in src/index.ts, with the other app.get()
+app.get("/api/profile/:userid", (req: Request, res: Response) => {
+  const { userid } = req.params;
+
+  profiles
+    .get(userid)
+    .then((profile: Profile) => res.json(profile))
+    .catch((err) => res.status(404).end());
+});
+
+// in src/index.ts, after the previous app.get()
+app.post("/api/profiles", (req: Request, res: Response) => {
+  const newProfile = req.body;
+
+  profiles
+    .create(newProfile)
+    .then((profile: Profile) => res.status(201).send(profile))
+    .catch((err) => res.status(500).send(err));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- packages/express-backend/src/mongoConnect.ts
+++ packages/express-backend/src/mongoConnect.ts
@@ -12,15 +12,17 @@ function getMongoURI(dbname: string) {
   if (MONGO_USER && MONGO_PWD && MONGO_CLUSTER) {
     console.log(
       "Connecting to MongoDB at",
-      `mongodb+srv://${MONGO_USER}:<password>@${MONGO_CLUSTER}/${dbname}`
+      `mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_CLUSTER}/${dbname}`
     );
-    connection_string = `mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_CLUSTER}/${dbname}?retryWrites=true&w=majority`;
+    connection_string = `mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_CLUSTER}?retryWrites=true&w=majority`;
   } else {
     console.log("Connecting to MongoDB at ", connection_string);
   }
+  console.log("Connection string", connection_string);
   return connection_string;
 }
 
 export function connect(dbname: string) {
+  // mongoose.createConnection(getMongoURI(dbname));
   mongoose.connect(getMongoURI(dbname)).catch((error) => console.log(error));
 }
